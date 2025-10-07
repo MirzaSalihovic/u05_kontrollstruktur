2a8485a3cafcbe940bf50756e07676aa264eb150
Mirza Salihovic <salihovic.mirza@web.de>
2025-10-07 14:46:13 +0200

    Create file Hochhaus2.java

---
+++ auf5.07/Hochhaus2.java
@@ -0,0 +1,27 @@
+public class Hochhaus2
+{
+   public static void main(String[] args)
+   {
+      System.out.println("        /\\");
+      System.out.println("       /  \\");
+      System.out.println("      /    \\");
+      System.out.println("     /      \\");
+      System.out.println("    /        \\");
+      System.out.println("   /          \\");
+      System.out.println("  /            \\");
+      System.out.println(" /______________\\");
+      System.out.println("/|              |\\");
+
+      for(int i = 1; i<=4; i++)
+      {
+      System.out.println(" |  ___    ___  | ");
+      System.out.println(" |  | |    | |  | ");
+      System.out.println(" |  ---    ---  | ");
+      }
+
+      System.out.println(" |  HOTEL  ___  | ");
+      System.out.println(" |  |   |  | |  | ");
+      System.out.println(" |  |   |  ---  | ");
+      System.out.println("===================");
+   }
+}
