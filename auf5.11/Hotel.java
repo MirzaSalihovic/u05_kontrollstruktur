87ce56c0253cfc2ed861e45cfbe79cf0ea40a1ed
Mirza Salihovic <salihovic.mirza@web.de>
2025-10-09 10:05:44 +0200

    Create file Hotel.java

--- auf5.11/Hotel.java
+++ auf5.11/Hotel.java
@@ -19,22 +19,27 @@ public class Hotel
       System.out.println(" |  ___    ___  | ");
       System.out.println(" |  | |    | |  | ");
       System.out.println(" |  ---    ---  | ");
+
       if(sterne == 1)
       {
-      System.out.println(" |Absteige ___  | ");
+         System.out.println(" |Absteige ___  | ");
       }
+
       if(sterne == 2)
       {
-      System.out.println(" |Gasthaus ___  | ");
+         System.out.println(" |Gasthaus ___  | ");
       }
+
       if(sterne == 3)
       {
-      System.out.println(" |  Hotel  ___  | ");
-      } 
+         System.out.println(" |  Hotel  ___  | ");
+      }
+
       if(sterne == 4)
       {
-      System.out.println(" |  Palast ___  | ");
-      } 
+         System.out.println(" |  Palast ___  | ");
+      }
+ 
       System.out.println(" |  |   |  | |  | ");
       System.out.println(" |  |   |  ---  | ");
       System.out.println("===================");
