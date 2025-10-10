258b0acce6a01d53abdecbfab628eb1af7bb227f
Mirza Salihovic <salihovic.mirza@web.de>
2025-10-10 09:50:20 +0200

    Create file Hotel2.java

---
+++ auf5.12/Hotel2.java
@@ -0,0 +1,57 @@
+// b05_kontrollstrukur
+// auf5.11
+
+public class Hotel2
+{
+   public static void main(String[] args)
+   {
+      int sterne = 5;
+
+      System.out.println("        /\\");
+      System.out.println("       /  \\");
+      System.out.println("      /    \\");
+      System.out.println("     /      \\");
+      System.out.println("    /        \\");
+      System.out.println("   /    ( )   \\");
+      System.out.println("  /            \\");
+      System.out.println(" /______________\\");
+      System.out.println("/|              |\\");
+      System.out.println(" |  ___    ___  | ");
+      System.out.println(" |  | |    | |  | ");
+      System.out.println(" |  ---    ---  | ");
+
+      if(sterne == 1)
+      {
+         System.out.println(" |Absteige ___  | ");
+      }
+      else
+      {
+         if(sterne == 2)
+         {
+            System.out.println(" |Gasthaus ___  | ");
+         }
+         else
+         {
+            if(sterne == 3)
+            {
+               System.out.println(" |  Hotel  ___  | ");
+            } 
+            else
+            {
+               if(sterne == 4)
+               {
+                  System.out.println(" |  Palast ___  | ");
+               }
+               else
+               {
+                  System.out.println(" |  _____  ___  | ");
+               }
+            }
+         }
+      }
+
+      System.out.println(" |  |   |  | |  | ");
+      System.out.println(" |  |   |  ---  | ");
+      System.out.println("===================");
+   }
+}
