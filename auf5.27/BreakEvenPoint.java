b23283a5685750b90736ce9286a022457bbcf002
Mirza Salihovic <salihovic.mirza@web.de>
2025-10-13 09:48:24 +0200

    Create file BreakEvenPoint.java

---
+++ auf5.27/BreakEvenPoint.java
@@ -0,0 +1,42 @@
+public class BreakEvenPoint
+{
+   public static void main(String[] args)
+   {
+
+      
+      int verkaufspreis  = 1500;
+      int rohstoffkosten = 600;
+      int arbeitskosten  = 400;
+      int fixkosten      = 5000;
+      int stueckzahl     = 15;
+      
+      System.out.println("Break-Even-Point");
+      System.out.println("================");
+      System.out.println("Verkaufspreis pro Stueck: " + verkaufspreis);
+      System.out.println("Rohstoffkosten pro Stueck: " + rohstoffkosten);
+      System.out.println("Arbeitskosten pro Stueck: " + arbeitskosten);
+      System.out.println("Fixkosten pro Monat: " + fixkosten);
+      System.out.println("Maximale Stueckzahl pro Monat: " + stueckzahl);
+
+      for(int i = 1; i<= stueckzahl; i++)
+      {
+         int variablekosten = (rohstoffkosten + arbeitskosten) * i;
+         int erloes = verkaufspreis * i;
+         int gesamtkosten = variablekosten + fixkosten;
+         int ueberschuss = erloes - gesamtkosten;
+         
+         System.out.println("Stueckzahl: " + i + " " + "Gesamtkosten: " + gesamtkosten + " " + "Erloes: " + erloes + " " + "Ueberschuss: " + ueberschuss);
+      }
+
+      //while(betrag > 100)
+      //{
+      // betrag -= 100;
+      // System.out.println(zähler++ + ". Rate: " + rate + "Euro");
+         
+        // if(betrag < 100)
+        // {
+           // System.out.println(zähler++ + ". Rate: " + betrag + "Euro");
+         //}
+      //}
+   }
+}
